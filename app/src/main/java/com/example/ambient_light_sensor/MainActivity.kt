6b9a83d68a381299f48a68204096a93bc7578528
Prince <banerjee.prince69@gmail.com>
2024-05-24 19:15:32 +0530

    Final Commit

--- app/src/main/java/com/example/ambient_light_sensor/MainActivity.kt
+++ app/src/main/java/com/example/ambient_light_sensor/MainActivity.kt
@@ -9,9 +9,34 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sensorManager = getSystemService(SENSOR_SERVICE) as? SensorManager
+        if (sensorManager != null) {
+            val ambientSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+            if (ambientSensor != null) {
+                sensorManager.registerListener(
+                    this,
+                    ambientSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL
+                )
+            }
+        } else {
+            Toast.makeText(this, "Sensor service not started", Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type==Sensor.TYPE_LIGHT) {
+            findViewById<TextView>(R.id.tv_sensor_values).text =
+                "X: ${event.values[0]}, Y: ${event.values[1]}, Z: ${event.values[2]}"
+        }
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }
