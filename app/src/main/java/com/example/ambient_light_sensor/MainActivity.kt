517064698aac55ee366045afae9f0d3d9ae8cb4d
Prince <banerjee.prince69@gmail.com>
2024-05-24 19:12:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ambient_light_sensor/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ambient_light_sensor
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
